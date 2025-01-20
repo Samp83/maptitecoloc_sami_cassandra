e77faa41e46fd635a2445fdfbbfc37133723f58a
Samp83 <sami.paris@supdevinci-edu.fr>
2025-01-20 16:08:29 +0100

    fix routes

--- src/routes/user/user.routes.ts
+++ src/routes/user/user.routes.ts
@@ -10,7 +10,11 @@ routes.post("/register", userController.registerUser);
 // Route pour la connexion d'un utilisateur
 routes.post("/login", userController.loginUser);
 
-// Route pour récupérer le profil de l'utilisateur connecté
+routes.get("/", userController.getAllUsers);
+routes.get("/:id", userController.getUserById);
+routes.put("/:id", userController.updateUser);
+routes.delete("/:id", userController.deleteUser);
+
 routes.get("/me", /* authenticate, userController.getUserProfile */);
 
 export default routes;
