014b2cad8e175000f00eaf7e9d0a5ca610803965
CassC17 <cassandra.cinna@supdevinci-edu.fr>
2025-01-20 10:27:31 +0100

    start crypting

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -7,20 +7,29 @@ import { UserPresenter } from "../types/user/presenters";
 
 const userService = new UserService();
 
-export const registerUser = async (req: Request, res: Response): Promise<void> => {
+export const registerUser = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
   try {
-   const userToCreateDTO = plainToInstance(UserToCreateDTO, req.body, { excludeExtraneousValues: true });
+    // transform userToCreateDTO en userToCreateInput (obj)
+    const userToCreateDTO = plainToInstance(UserToCreateDTO, req.body, {
+      excludeExtraneousValues: true,
+    });
 
-   const dtoErrors = await validate(userToCreateDTO);
-   if (dtoErrors.length > 0) {
-     console.log(dtoErrors);
-     throw new Error("Invalid fields");
-   }
-    
+    //Controle des données
+    const dtoErrors = await validate(userToCreateDTO);
+    if (dtoErrors.length > 0) {
+      console.log(dtoErrors);
+      throw new Error("Invalid fields");
+    }
+
     const user = await userService.registerUser(req.body);
     // appeler le logger service pour enregistrer QUI a créer un utilisateur (peut être un admin ou l'utilisateur lui même (?)  )
 
-    const createdUser = plainToInstance(UserPresenter, user, { excludeExtraneousValues: true });
+    const createdUser = plainToInstance(UserPresenter, user, {
+      excludeExtraneousValues: true,
+    });
     res.status(201).json(createdUser); // à vous de créer une class pour gérer les success
   } catch (error) {
     throw error;
