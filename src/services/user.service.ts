d2e6e15d20d7b89ed00ed269f1a846ba2ff80fa8
CassC17 <cassandra.cinna@supdevinci-edu.fr>
2025-01-20 14:33:36 +0100

    complete user service

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -2,6 +2,8 @@ import { UserEntity } from "../databases/mysql/user.entity";
 import { UserRepository } from "../repositories/user.repository";
 import { UserToCreateDTO } from "../types/user/dtos";
 import bcrypt from "bcrypt";
+import { UserPresenter } from "../types/user/presenters";
+import { userToCreateInput } from "../types/user/Inputs";
 
 export class UserService {
   private userRepository = new UserRepository();
@@ -11,8 +13,6 @@ export class UserService {
 
     // ON HASH LE MOT DE PASSE
     const password_hash = await bcrypt.hash(userToCreate.password, 10);
-    
-    
 
     // ON CRÉE L'UTILISATEUR
     const createdUser = this.userRepository.create({
@@ -27,5 +27,37 @@ export class UserService {
 
     // ON RETOURNE L'UTILISATEUR CRÉÉ
     return savedUser;
+  }
+
+  async getUserById(id: number): Promise<UserPresenter | null> {
+    const user = await this.userRepository.findById(id);
+    if (!user) return null;
+    return {
+      id: user.id,
+      firstname: user.firstname,
+      lastname: user.lastname,
+      email: user.email,
+      is18: user.is18,
+      isActive: user.isActive,
+    };
+  }
+
+  async getAllUsers(): Promise<UserPresenter[]> {
+    return this.userRepository.findAll();
+  }
+
+  async updateUser(
+    id: number,
+    updatedData: Partial<userToCreateInput>
+  ): Promise<void> {
+    const update = await this.userRepository.update(id, updatedData);
+    if (update === null) {
+      throw new Error("User not found");
+    }
+    return;
+  }
+
+  async deleteUser(id: number): Promise<void> {
+    return await this.userRepository.delete(id);
   }
 }
--- src/types/user/presenters.ts
+++ src/types/user/presenters.ts
@@ -1,24 +1,34 @@
 import { Expose } from "class-transformer";
-import { IsNumber, IsString, IsEmail, IsBoolean, MinLength } from "class-validator";
+import {
+  IsNumber,
+  IsString,
+  IsEmail,
+  IsBoolean,
+  MinLength,
+} from "class-validator";
 import { UserEntity } from "../../databases/mysql/user.entity";
 
 export class UserPresenter {
-   @Expose()
-   @IsString()
-   firstname: UserEntity['firstname'];
- 
-   @Expose()
-   @IsString()
-   lastname: UserEntity['lastname'];
- 
-   @Expose()
-   @IsEmail()
-   email: UserEntity['email'];
- 
-   @Expose()
-   @IsBoolean()
-   is18: UserEntity['is18'];
-  
-   @IsBoolean()
-   isActive: UserEntity['isActive'];
+  @Expose()
+  @IsNumber()
+  id: UserEntity["id"];
+
+  @Expose()
+  @IsString()
+  firstname: UserEntity["firstname"];
+
+  @Expose()
+  @IsString()
+  lastname: UserEntity["lastname"];
+
+  @Expose()
+  @IsEmail()
+  email: UserEntity["email"];
+
+  @Expose()
+  @IsBoolean()
+  is18: UserEntity["is18"];
+
+  @IsBoolean()
+  isActive: UserEntity["isActive"];
 }
