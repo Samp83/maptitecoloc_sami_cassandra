93f203fd0b0719aa0c98e2721bc7d92e81f45d2d
Samp83 <sami.paris@supdevinci-edu.fr>
2025-01-20 10:14:32 +0100

    did user presenter

--- src/types/user/presenters.ts
+++ src/types/user/presenters.ts
@@ -1,18 +1,24 @@
 import { Expose } from "class-transformer";
-import { IsNumber, IsString } from "class-validator";
+import { IsNumber, IsString, IsEmail, IsBoolean, MinLength } from "class-validator";
 import { UserEntity } from "../../databases/mysql/user.entity";
 
 export class UserPresenter {
-  @Expose()
-  @IsNumber()
-  id: UserEntity['id'];
-
-  @Expose()
-  @IsString()
-  firstname: UserEntity['firstname'];
-
-  // à vous de jouer
-  lastname: string;
-  email: string;
-  isActive: boolean;
+   @Expose()
+   @IsString()
+   firstname: UserEntity['firstname'];
+ 
+   @Expose()
+   @IsString()
+   lastname: UserEntity['lastname'];
+ 
+   @Expose()
+   @IsEmail()
+   email: UserEntity['email'];
+ 
+   @Expose()
+   @IsBoolean()
+   is18: boolean;
+  
+   @IsBoolean()
+   isActive: boolean;
 }
