573a5663f3b63d41f5911aed48e06491af2031a3
CassC17 <cassandra.cinna@supdevinci-edu.fr>
2025-01-21 13:28:17 +0100

    log/task model

--- src/databases/mongodb/task.model.ts
+++ src/databases/mongodb/task.model.ts
@@ -8,9 +8,23 @@ export interface ITask extends Document {
 }
 
 const TaskSchema: Schema = new Schema({
-  name: { type: String, required: true },
+  name: {
+    type: String,
+    required: true,
+    enum: [
+      "CREATE_USER",
+      "DELETE_USER",
+      "UPDATE_USER",
+      "CREATE_COLOC",
+      "DELETE_COLOC",
+      "ADD_MEMBER",
+      "REMOVE_MEMBER",
+      "UPDATE_COLOC",
+      "TRANSFER_COLOC",
+    ],
+  }, //voir si + finance
   assignedTo: { type: String, required: true },
-  dueDate: { type: Date, required: true },
+  dueDate: { type: Date, required: true, default: Date.now },
   status: { type: String, enum: ["pending", "completed"], default: "pending" },
 });
 
